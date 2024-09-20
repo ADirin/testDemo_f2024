5f2bf7a7d2333084b0bf870b1ea1c95a8fa31297
amirdi <amir.dirin@yahoo.com>
2024-09-20 08:30:55 +0300

    Initial Commit

---
+++ src/test/java/AppTest.java
@@ -0,0 +1,16 @@
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
+public class AppTest {
+    App app = new App();
+    @Test
+    public void addMeTest(){
+        assertEquals(12,app.addMe(11,1) );
+
+    }
+    @Test
+    public void subMeTest(){
+        assertEquals(12,app.subMe(13,1) );
+    }
+}
